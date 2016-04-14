783af060321748098c1b7e45a9cc6dfe7fe77a33
Tarun Gupta Akirala <takirala@ufl.edu>
2016-04-14 16:11:55 -0400

    Added role based auth

--- app/src/main/java/smartfoodcluster/feedme/auth/LoginActivity.java
+++ app/src/main/java/smartfoodcluster/feedme/auth/LoginActivity.java
@@ -4,6 +4,7 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import android.view.View.OnClickListener;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,36 +26,14 @@ import com.google.android.gms.common.SignInButton;
 import smartfoodcluster.feedme.R;
 import smartfoodcluster.feedme.handlers.AbstractGetNameTask;
 import smartfoodcluster.feedme.handlers.GetNameInForeground;
+import smartfoodcluster.feedme.restaurateur.RestaurateurHome;
 import smartfoodcluster.feedme.user.UserHome;
 import smartfoodcluster.feedme.util.Constants;
 
 /**
- * A login screen that offers login via email/password.
+ * A login screen that offers login via Google account
  */
 public class LoginActivity extends AppCompatActivity {
-
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    //private UserLoginTask mAuthTask = null;
-
-    // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
 
     Context mContext = LoginActivity.this;
     AccountManager mAccountManager;
@@ -66,17 +46,35 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        SharedPreferences settings = getSharedPreferences(Constants.PREFS_NAME, 0);
+        final boolean userAuthenticated = settings.getBoolean(Constants.userAuthenticated, false);
+        if (userAuthenticated) {
+            boolean userRole = settings.getBoolean(Constants.userRole, Constants.UserRole.USER);
+            navigateToNextPage(userRole);
+            return;
+        }
+
         SignInButton mEmailSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                RadioGroup userRoleGroup = (RadioGroup) findViewById(R.id.userRole);
+                if (userRoleGroup.getCheckedRadioButtonId() == -1) {
+                    Toast.makeText(LoginActivity.this, "Please select a role for yourself before authenticating. This can be changed later!!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                boolean userRole = Constants.UserRole.USER;
+                if (findViewById(userRoleGroup.getCheckedRadioButtonId()).getId() == R.id.owner) {
+                    userRole = Constants.UserRole.OWNER;
+                } else {
+                    userRole = Constants.UserRole.USER;
+                }
+                Log.e("Auth / uSer role ", ">>" + userRole);
                 boolean authSuccess = true;
                 if (Constants.authEnabled) authSuccess = syncGoogleAccount();
                 if (authSuccess) {
-                    Intent i = new Intent(getApplicationContext(), UserHome.class);
-                    i.putExtra(Constants.showSuccess, true);
-                    startActivity(i);
-                    setContentView(R.layout.activity_user_home);
+                    navigateToNextPage(userRole);
                 } else {
                     try {
                         Thread.sleep(2000);
@@ -86,7 +84,20 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
 
+    private void navigateToNextPage(boolean userRole) {
+        if (userRole == Constants.UserRole.USER) {
+            Intent i = new Intent(getApplicationContext(), UserHome.class);
+            i.putExtra(Constants.showSuccess, true);
+            startActivity(i);
+            setContentView(R.layout.activity_user_home);
+        } else {
+            Intent i = new Intent(getApplicationContext(), RestaurateurHome.class);
+            i.putExtra(Constants.showSuccess, true);
+            startActivity(i);
+            setContentView(R.layout.activity_restaurateur_home);
+        }
     }
 
     private String[] getAccountNames() {
