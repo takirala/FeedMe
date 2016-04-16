0b4cfd3f2ccc025768f0543a274e774e738bf522
Tarun Gupta Akirala <takirala@ufl.edu>
2016-04-16 19:13:11 -0400

    Adding user endpoint

---
+++ backend/src/main/java/smartfoodcluster/feedme/util/User.java
@@ -0,0 +1,62 @@
+package smartfoodcluster.feedme.util;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+/**
+ * Created by Lavenger on 4/16/2016.
+ */
+@Entity
+public class User {
+
+    @Id
+    Long id;
+    String email;
+    String role;
+    String address;
+    String name;
+
+    User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
